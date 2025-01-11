27e23dbd513f1bfd8c7703846ef4cf500658052a
Yoel <yoelsolca5@gmail.com>
2025-01-11 17:04:16 -0300

    Add project files.

---
+++ Pacagroup.Ecommerce.Services.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Application.Interface;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly ICustomersApplication _customersApplication;
+        public CustomersController(ICustomersApplication customersApplication)
+        {
+            _customersApplication = customersApplication;
+        }
+
+        #region "Métodos Sincronos"
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+            var response = _customersApplication.Insert(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody]CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+            var response = _customersApplication.Update(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public IActionResult Delete(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+            var response = _customersApplication.Delete(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public IActionResult Get(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+            var response = _customersApplication.Get(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var response = _customersApplication.GetAll();
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+        #endregion
+
+        #region "Métodos Asincronos"
+
+        [HttpPost]
+        public async Task<IActionResult> InsertAsync([FromBody]CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+            var response = await _customersApplication.InsertAsync(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody]CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+            var response = await _customersApplication.UpdateAsync(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public async Task<IActionResult> DeleteAsync(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+            var response = await _customersApplication.DeleteAsync(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetAsync(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+            var response = await _customersApplication.GetAsync(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _customersApplication.GetAllAsync();
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Pacagroup.Ecommerce.Services.WebApi/Startup.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using AutoMapper;
+using Pacagroup.Ecommerce.Transversal.Mapper;
+using Pacagroup.Ecommerce.Transversal.Common;
+using Pacagroup.Ecommerce.Infrastructure.Data;
+using Pacagroup.Ecommerce.Infrastructure.Repository;
+using Pacagroup.Ecommerce.Infrastructure.Interface;
+using Pacagroup.Ecommerce.Domain.Interface;
+using Pacagroup.Ecommerce.Domain.Core;
+using Pacagroup.Ecommerce.Application.Interface;
+using Pacagroup.Ecommerce.Application.Main;
+using Pacagroup.Ecommerce.Transversal.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using System.IO;
+using Pacagroup.Ecommerce.Services.WebApi.Helpers;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Pacagroup.Ecommerce.Services.WebApi
+{
+    public class Startup
+    {
+        readonly string myPolicy = "policyApiEcommerce";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
+            services.AddCors(options => options.AddPolicy(myPolicy, builder => builder.WithOrigins(Configuration["Config:OriginCors"])
+                                                                                        .AllowAnyHeader()
+                                                                                        .AllowAnyMethod()));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => { options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver(); });
+
+            var appSettingsSection = Configuration.GetSection("Config");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+            services.AddScoped<ICustomersApplication, CustomersApplication>();
+            services.AddScoped<ICustomersDomain, CustomersDomain>();
+            services.AddScoped<ICustomersRepository, CustomersRepository>();
+            services.AddScoped<IUsersApplication, UsersApplication>();
+            services.AddScoped<IUsersDomain, UsersDomain>();
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            var Issuer = appSettings.Issuer;
+            var Audience = appSettings.Audience;
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var userId = int.Parse(context.Principal.Identity.Name);
+                        return Task.CompletedTask;
+                    },
+
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        {
+                            context.Response.Headers.Add("Token-Expired", "true");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = false;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = Audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Pacagroup Technology Services API Market",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Alex Espejo",
+                        Email = "alex.espejo.c@gmail.com",
+                        Url = "https://pacagroup.com"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                c.AddSecurityDefinition("Authorization", new ApiKeyScheme
+                {
+                    Description = "Authorization by API key.",
+                    In = "header",
+                    Type = "apiKey",
+                    Name = "Authorization"
+                });
+
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    { "Authorization", new string[0] }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API Ecommerce V1");
+            });
+
+            app.UseCors(myPolicy);
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
