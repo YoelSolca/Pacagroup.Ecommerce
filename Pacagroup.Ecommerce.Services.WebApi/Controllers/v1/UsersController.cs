34f974ba3d7fc786dfc413143f897c2384fdea0c
yoelsolca5@gmail.com <yoelsolca5@gmail.com>
2025-01-18 14:39:16 -0300

    Control de Versiones utilizando Parámetros en el Segmento de la URL

--- Pacagroup.Ecommerce.Services.WebApi/Controllers/v1/UsersController.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Controllers/v1/UsersController.cs
@@ -14,7 +14,7 @@ using System.Text;
 namespace Pacagroup.Ecommerce.Services.WebApi.Controllers.v1
 {
     [Authorize]
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     [ApiVersion("1.0")]
     public class UsersController : Controller
--- Pacagroup.Ecommerce.Services.WebApi/Modules/Versioning/VersioningExtensions.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Modules/Versioning/VersioningExtensions.cs
@@ -12,13 +12,13 @@ namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Versioning
                 o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
                 o.AssumeDefaultVersionWhenUnspecified = true;
                 o.ReportApiVersions = true;
-                //o.ApiVersionReader = new QueryStringApiVersionReader("api-version");
-                o.ApiVersionReader = new HeaderApiVersionReader("x-version");
+                o.ApiVersionReader = new UrlSegmentApiVersionReader();
             });
 
             services.AddVersionedApiExplorer(options =>
             {
                 options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
             });
 
             return services;
