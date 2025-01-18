ae341c7e9695c42968df9e44cda27ee7f7859cc3
YoelSolca <yoelsolca5@gmail.com>
2025-01-18 20:40:24 -0300

    Implementación del Patrón Repository

--- Pacagroup.Ecommerce.Services.WebApi/Modules/Injection/InjectionExtensions.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Modules/Injection/InjectionExtensions.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Pacagroup.Ecommerce.Transversal.Common;
-using Pacagroup.Ecommerce.Infrastructure.Data;
-using Pacagroup.Ecommerce.Infrastructure.Repository;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Pacagroup.Ecommerce.Application.Interface;
 using Pacagroup.Ecommerce.Application.Main;
-using Pacagroup.Ecommerce.Domain.Interface;
 using Pacagroup.Ecommerce.Domain.Core;
+using Pacagroup.Ecommerce.Domain.Interface;
+using Pacagroup.Ecommerce.Infrastructure.Data;
 using Pacagroup.Ecommerce.Infrastructure.Interface;
+using Pacagroup.Ecommerce.Infrastructure.Repository;
+using Pacagroup.Ecommerce.Transversal.Common;
 using Pacagroup.Ecommerce.Transversal.Logging;
-using Microsoft.Extensions.Configuration;
 
 namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Injection
 {
@@ -17,7 +17,7 @@ namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Injection
         public static IServiceCollection AddInjection(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IConfiguration>(configuration);
-            services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+            services.AddSingleton<DapperContext>();
             services.AddScoped<ICustomersApplication, CustomersApplication>();
             services.AddScoped<ICustomersDomain, CustomersDomain>();
             services.AddScoped<ICustomersRepository, CustomersRepository>();
